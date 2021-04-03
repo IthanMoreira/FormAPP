bf0787a8c37971655140a1c401bf4fac40114c07
Ithan Moreira <ithan.moreira@gmail.com>
2021-04-03 20:40:14 -0300

    Update settings.py

--- formulaAPP/settings.py
+++ formulaAPP/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v@h9e-9!2(ne)=dj!c!fi5(q*%19m3_flh*4++9&wvei06t0eq'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
